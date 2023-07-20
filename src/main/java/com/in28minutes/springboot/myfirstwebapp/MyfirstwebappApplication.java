50ef4d0c91facacb1ed9faf7a015a1995dd4c354
Kat Hugh <kathleen.hugh@kmart.com.au>
2023-07-20 10:51:38 +1000

    first commit

---
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/MyfirstwebappApplication.java
@@ -0,0 +1,21 @@
+package com.in28minutes.springboot.myfirstwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstwebappApplication.class, args);
+	}
+
+}
+
+
+
+//Notes
+//Ctrl + space
+//Ctrl + 1
+
+//https://docs.spring.io/spring-framework/docs/3.2.x/spring-framework-reference/html/view.html
